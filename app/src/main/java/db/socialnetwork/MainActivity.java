84f4ced7b4d739ee01292937923deca1c312c346
Hari Prasad V <hariv97@gmail.com>
2017-10-08 20:42:38 +0530

    Added the project

---
+++ app/src/main/java/db/socialnetwork/MainActivity.java
@@ -0,0 +1,31 @@
+package db.socialnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printLog(View view){
+        EditText name = (EditText) findViewById(R.id.userName);
+        String uname = name.getText().toString();
+        EditText password = (EditText) findViewById(R.id.pwd);
+        String upwd = password.getText().toString();
+        if(uname.equals("")||upwd.equals("")){
+            Toast.makeText(getApplicationContext(), "Fill all fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.v("UserName",uname);
+        Log.v("Password",upwd);
+    }
+}
