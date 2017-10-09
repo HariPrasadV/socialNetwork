c66e8c085db45392582c051df84772020c488b47
Jeyasoorya <jeys041297sts@gmail.com>
2017-10-09 12:32:20 +0530

    Login response string printed in log

--- app/src/main/java/db/socialnetwork/MainActivity.java
+++ app/src/main/java/db/socialnetwork/MainActivity.java
@@ -1,5 +1,8 @@
 package db.socialnetwork;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -27,5 +30,70 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.v("UserName",uname);
         Log.v("Password",upwd);
+        new ValidateUser().execute(uname, upwd);
+    }
+
+    private class ValidateUser extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... arg) {
+            String url = "http://192.168.0.102:8080/SocialBackend/Login";
+            String uname = arg[0];
+            String pwd = arg[1];
+            ServiceHandler s = new ServiceHandler();
+            String msg = "";
+            try {
+                msg = s.authorizationCall(url, uname, pwd);
+                if (msg.equals("Invalid")) {
+                    return "__invalidcre__";
+                }
+                else if(msg.equals("Connection Error")) {
+                    return "__invalidc__";
+                }
+
+                return msg;
+                //
+            } catch (Exception e) {
+                Log.v("MyUser:",e.getMessage());
+                return "__invalid__";
+            }
+
+
+        }
+
+
+        protected void onPostExecute(String result) {
+            //   Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            if(result.equals("__invalidcre__")){
+                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_LONG).show();
+            }
+            else if(result.equals("__invalidc__")){
+                Toast.makeText(getApplicationContext(), "Connection Error", Toast.LENGTH_LONG).show();
+            }
+            else if(result.equals("__invalid__")){
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Log.v("Result",result);
+//               SharedPreferences s =(getApplicationContext()).getSharedPreferences("Myprefs",MODE_PRIVATE);
+//                SharedPreferences.Editor e = s.edit();
+//                e.putString("uname",result);
+//                e.commit();
+//                Toast toast = Toast.makeText(getApplicationContext(), "Welcome" + result, Toast.LENGTH_SHORT);
+//                toast.show();
+                //Log.v("HariPrasad",result);
+//                Intent nextScreen = new Intent(getApplicationContext(), Main2Activity.class);
+//                nextScreen.putExtra("uname", result);
+//                nextScreen.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                nextScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                nextScreen.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//                startActivity(nextScreen);
+            }
+
+        }
+
     }
 }
---
+++ app/src/main/java/db/socialnetwork/ServiceHandler.java
@@ -0,0 +1,78 @@
+package db.socialnetwork;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+/**
+ * Created by jeyasoorya on 9/10/17.
+ */
+
+public class ServiceHandler {
+    public String authorizationCall(String url, String uname, String pwd)throws Exception{
+        JSONObject params=new JSONObject();
+        params.put("id",uname);
+        params.put("password",pwd);
+        return makeServiceCall(params,url);
+    }
+    public String makeServiceCall(JSONObject params, String url)throws Exception{
+        URL site = new URL(url);
+        HttpURLConnection urlConnection = (HttpURLConnection) site.openConnection();
+        urlConnection.setRequestMethod("POST");
+        urlConnection.setDoInput(true);
+        urlConnection.setDoOutput(true);
+        OutputStream out = urlConnection.getOutputStream();
+        BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(out,"UTF-8"));
+        writer.write(getPostDataString(params));
+        writer.flush();
+        writer.close();
+        out.close();
+        urlConnection.connect();
+        int response = urlConnection.getResponseCode();
+        if(response== HttpURLConnection.HTTP_OK){
+            BufferedReader in=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuffer sb=new StringBuffer("");
+            String line="";
+            while((line=in.readLine())!=null){
+                sb.append(line);
+                break;
+            }
+            in.close();
+            urlConnection.disconnect();
+            Log.v("Response",sb.toString());
+            return sb.toString();
+        }
+        else{
+            Log.v("Response Code", String.valueOf(response));
+            return new String("Connection Error");
+        }
+
+    }
+    public String getPostDataString(JSONObject params)throws Exception{
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        Iterator<String> itr = params.keys();
+        while(itr.hasNext()){
+            String key=itr.next();
+            Object value=params.get(key);
+            if(first)
+                first=false;
+            else
+                sb.append("&");
+            sb.append(URLEncoder.encode(key,"UTF-8"));
+            sb.append("=");
+            sb.append(URLEncoder.encode(value.toString(),"UTF-8"));
+        }
+        return sb.toString();
+    }
+}
